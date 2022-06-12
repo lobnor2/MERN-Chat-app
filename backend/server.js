417c59168f69ad9802f400cc37df99478e267b63
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-06-12 12:11:48 +0530

    first express server created on port 5000

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+// instance of express
+const app = express();
+
+// you can choose any port number
+app.listen(5000, console.log("server is listening at port 5000"));
