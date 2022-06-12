5b3484ef01942b766c2ef93d950989e3f21d7e75
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-06-12 12:59:32 +0530

    added nodemon and tested api

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,32 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
 
 // instance of express
 const app = express();
 
+dotenv.config();
+
+// first express api
+app.get("/", (req, res) => {
+  res.send("api is running succesfully");
+});
+
+// complex api and dummy data at /api/chat
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+// to get singlechat using params id and respond
+app.get("/api/chat/:id", (req, res) => {
+  //   console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
 // you can choose any port number
-app.listen(5000, console.log("server is listening at port 5000"));
+// we dont want to make port 5000 public so make a .envfile and create varible PORT = 5000 and install dotevn
+//import dotenv and config
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`server is listening at port ${PORT}`));
